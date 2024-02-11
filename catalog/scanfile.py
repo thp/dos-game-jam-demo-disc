f2f2b2dda83831c0a4038a7984e2a7c8339dcca6
Thomas Perl <m@thp.io>
2024-02-11 08:12:56 +0100

    scanfile.py: More checks

--- catalog/scanfile.py
+++ catalog/scanfile.py
@@ -3,15 +3,47 @@
 
 import os
 import itertools
+import string
+
+def valid_char(ch):
+    return ch in (string.digits + string.ascii_lowercase + '_')
+
+BLOCKLISTED_FILENAMES = ('cwsdpmi.swp',)
+
+def check_filename(basename):
+    basename = basename.lower()
+
+    if basename in BLOCKLISTED_FILENAMES:
+        # Temporary/swap/... file
+        return False
+
+    if '.' in basename:
+        base, ext = basename.split('.', 1)
+        if '.' in ext:
+            # Multiple dots in filename
+            return False
+    else:
+        base, ext = basename, ''
+
+    if len(base) > 8:
+        # Basename longer than 8 characters
+        return False
+
+    if len(ext) > 3:
+        # Extension longer than 3 characters
+        return False
+
+    if not all(valid_char(ch) for ch in (base + ext)):
+        # Invalid characters
+        return False
+
+    return True
+
+
+
 
 for basedir in ('DEMO2023', 'pcx', 'extras'):
     for dirname, filenames, dirnames in os.walk(basedir):
         for filename in itertools.chain(filenames, dirnames):
-            if filename.lower() == 'cwsdpmi.swp':
-                print('/'.join((dirname, filename)))
-            if '.' in filename:
-                base, ext = filename.split('.', 1)
-                if len(base) > 8 or len(ext) > 3:
-                    print('/'.join((dirname, filename)))
-            if '-' in filename:
+            if not check_filename(filename):
                 print('/'.join((dirname, filename)))
