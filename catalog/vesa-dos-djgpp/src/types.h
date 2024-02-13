acbfce1ae61ce88dc10f6dd5e26c4ef97c5ef9d6
Thomas Perl <m@thp.io>
2024-02-13 06:06:43 +0100

    Add missing vesa-dos-djgpp

---
+++ catalog/vesa-dos-djgpp/src/types.h
@@ -0,0 +1,12 @@
+#ifndef __TYPES_H_INCLUDED
+#define __TYPES_H_INCLUDED
+
+#define ushort  unsigned short
+#define ulong   unsigned long
+#define uchar   unsigned char
+#define uint    unsigned int
+#define dword   signed long
+#define word    signed short
+#define byte    signed char
+
+#endif
